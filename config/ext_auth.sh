bf3025d2a61da964cedb7e5eaf8608b5f93b5347
tyage <namatyage@gmail.com>
2018-10-28 02:31:35 +0900

    remove broken condition

--- config/ext_auth.sh
+++ config/ext_auth.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ ! -z "$BULLSEYE_WEB_URL" ]; then
-    BULLSEYE_WEB_URL="http://localhost:3000"
-fi
-
 read username password
 
 result=`wget -O- $BULLSEYE_WEB_URL/internal_api/v1/login.json --header "Authorization: Bearer $BULLSEYE_API_AUTH_TOKEN" --post-data "name=$username&password=$password"`
